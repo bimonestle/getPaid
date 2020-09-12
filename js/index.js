37bb0401e50a648b5a97c51e05131cc6c5442b69
bimonestle <bimonestle@yahoo.co.id>
2020-09-12 22:11:35 +0700

    add typed.js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var options = {
+    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+    typeSpeed: 40,
+    // loop: true,
+    // loopCount: 4,
+    showCursor: true,
+    cursorChar: '|',
+    autoInsertCss: true
+  };
+  
+  var typed = new Typed('#terminal-typing', options);
