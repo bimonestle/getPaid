df23480cd84fa4af44f4da8ba9762ebd50b046ee
bimonestle <bimonestle@yahoo.co.id>
2020-09-12 22:59:31 +0700

    Typed JS done. Waiting for content. Terminal font use <pre>

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 var options = {
-    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+    strings: ['$ login:^1500 081295578742^1500\n$ Enter OTP:^2000  *^1000**^1000*\n\n Menu:^2500\n`1. Create VA\n2. Open Report\n3. Quit`\n:^7000'],
     typeSpeed: 40,
-    // loop: true,
-    // loopCount: 4,
+    loop: true,
+    loopCount: 4,
     showCursor: true,
     cursorChar: '|',
     autoInsertCss: true
